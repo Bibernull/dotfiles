03c75a8d2e6d088e40fbecb788368e8a00d95d27
Ivan Poštić <biber.np@gmail.com>
2026-08-30 00:56:29 +0200

    overwrite existing LESSOPEN env var if lessfilter exists

--- src/zprofile.zsh
+++ src/zprofile.zsh
@@ -66,12 +66,10 @@ fi
 
 # Set the Less input preprocessor.
 # Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
-if [[ -z "$LESSOPEN" ]]; then
-  if [[ -x "$HOME/.lessfilter" ]]; then
-    export LESSOPEN='|~/.lessfilter %s'
-  elif (( $#commands[(i)lesspipe(|.sh)] )); then
-    export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
-  fi
+if [[ -x "$HOME/.lessfilter" ]]; then
+  export LESSOPEN='|~/.lessfilter %s'
+elif [[ -z "$LESSOPEN" ]] && (( $#commands[(i)lesspipe(|.sh)] )); then
+  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
 fi
 
 (( $+commands[mise] )) && eval "$(mise activate zsh --shims)"
