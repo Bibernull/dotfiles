785740b5af23dfa909c6878a237b5c8ad7d81368
Ivan Poštić <biber.np@gmail.com>
2026-09-08 19:34:49 +0200

    add eza repo to apt sources on older ubuntu releases

--- setup.zsh
+++ setup.zsh
@@ -11,4 +11,15 @@ eval "$(${HOME}/.local/bin/mise activate zsh)"
 mise trust -y
 mise use -y -g usage bat-extras
 
+if command -v dpkg-query >/dev/null 2>&1 && ! dpkg-query -p eza >/dev/null 2>&1; then
+    apt update
+    apt install -y gpg
+
+    mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+    chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    apt update
+fi
+
 mise bootstrap -y
