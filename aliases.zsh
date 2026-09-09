4a3e28f4d1c07d94dec6464f946ad224ff6316c6
Ivan Poštić <biber.np@gmail.com>
2026-09-09 18:40:45 +0200

    move mise-pull to an external script

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,5 @@
 alias reload="exec $SHELL -l"
 alias l='"ls" -AlFh --color --group-directories-first'
-alias mise-pull="mise bootstrap --update --yes --from https://github.com/Bibernull/dotfiles.git"
 
 (( $+commands[batcat] )) && alias bat='batcat'
 (( $+commands[batcat] )) && alias cat='batcat'
